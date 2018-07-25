b976e4f3e5bb2ff37eea6509014b782b4eccf37a
Simone Basso <bassosimone@gmail.com>
2018-07-25 10:52:10 +0200

    [ci skip] fully comply with MIT license

--- libndt.cpp
+++ libndt.cpp
@@ -1117,6 +1117,28 @@ bool Client::msg_read_legacy(MsgType *code, std::string *msg) noexcept {
 // BIO that possibility comes out very easily anyway.)
 //
 // We assume that a OpenSSL 1.1.0-like API is available.
+/*-
+ * Portions created by Alan Antonuk are Copyright (c) 2017 Alan Antonuk.
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 
 // Helper used to route read and write calls to Client's I/O methods. We
 // disregard the const qualifier of the `base` argument. That is not a big
